0c947748f49a26d7b157a4c17ff9cd9a3ad869ba
Charles-Andre Bouffard <cabouffard@gmail.com>
2018-01-26 11:14:22 -0500

    Remove unecessary IF clause for the environment ENV variable
    This is not necessary as we have a default value if set to nothing.

--- luigi/contrib/datadog.py
+++ luigi/contrib/datadog.py
@@ -102,11 +102,10 @@ class DataDogMetricsCollector(MetricsCollector):
     def default_event_tags(self):
         default_tags = []
 
+        env_tag = "env={environment}".format(environment=self._config.environment)
+        default_tags.append(env_tag)
+
         if self._config.default_event_tags:
             default_tags = default_tags + str.split(self._config.default_event_tags, ',')
-
-        if self._config.environment:
-            env_tag = "env={environment}".format(environment=self._config.environment)
-            default_tags.append(env_tag)
 
         return default_tags
